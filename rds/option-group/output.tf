89c6024a9ef770fe65ca267effe4c60edadabd75
fejlaw <102074341+fejlaw@users.noreply.github.com>
2023-09-17 17:27:07 -0500

    updated rds module
    * added new modules
    
    * removed s3 outputs
    
    * fixed bad var name
    
    * removed unnecessary defaults
    
    * added efs module
    
    * Added ssm parameter module
    
    * Added elasticsearch module
    
    * updated rds module

---
+++ rds/option-group/output.tf
@@ -0,0 +1,9 @@
+output "db_option_group_id" {
+  description = "The db option group id"
+  value       = aws_db_option_group.this.id
+}
+
+output "db_option_group_arn" {
+  description = "The ARN of the db option group"
+  value       = aws_db_option_group.this.arn
+}
---
+++ rds/parameter-group/main.tf
@@ -0,0 +1,33 @@
+locals {
+  name        = var.use_name_prefix ? null : var.name
+  name_prefix = var.use_name_prefix ? "${var.name}-" : null
+
+  description = coalesce(var.description, format("%s parameter group", var.name))
+}
+
+resource "aws_db_parameter_group" "this" {
+  name        = local.name
+  name_prefix = local.name_prefix
+  description = local.description
+  family      = var.family
+
+  dynamic "parameter" {
+    for_each = var.parameters
+    content {
+      name         = parameter.value.name
+      value        = parameter.value.value
+      apply_method = lookup(parameter.value, "apply_method", null)
+    }
+  }
+
+  tags = merge(
+    var.tags,
+    {
+      "Name" = var.name
+    },
+  )
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
